131cd7ac886ee7ecf1e4a14c7ef66fc2206a59d0
leandrl-pixel <leandrl@uci.edu>
2026-04-29 23:51:47 -0700

    added more to my read me and scripts

--- Vertical Slice/Assets/Scenes - Main/Scripts - main/Traps.cs
+++ Vertical Slice/Assets/Scenes - Main/Scripts - main/Traps.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Traps : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int damage = 10; 
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        Debug.Log("Something collided with trap:" + collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Player hit the trap");
+            PlayerHealth health = collision.gameObject.GetComponent<PlayerHealth>();
+            if (health != null)
+            {
+                health.Takedamage(damage);
+            }
+            else
+            {
+                Debug.Log("No playerHealth script found");
+            }
+        }
     }
 }
