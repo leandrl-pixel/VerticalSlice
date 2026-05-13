1870ee49ac11abd632a7aedd4f6e44500195e9ae
leandrl-pixel <leandrl@uci.edu>
2026-05-13 16:54:56 -0700

    im assuming this is when I moved folders to different spots for organzing purposes

--- Vertical Slice/Assets/Scenes - Main/Scripts - main/Player Health.cs
+++ Vertical Slice/Assets/Scenes - Main/Scripts - main/Player Health.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//using TMPro; 
 
 public class PlayerHealth : MonoBehaviour
 {
 
     public float MaxHealth = 100;
     public float currentHealth;
-   // public Text healthtext;
-   // il probably make a text health ui later this is lowkey tough
+    // public Text healthtext;
+    // il probably make a text health ui later this is lowkey tough
+    public Transform respawnPoint; 
 
     private void Start()
     {
