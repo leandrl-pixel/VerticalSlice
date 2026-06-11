4ca949de6567887e53008553e1fdcb572c109718
leandrl-pixel <leandrl@uci.edu>
2026-06-11 00:52:34 -0700

    added audio in visual scripting for dissolve

--- Vertical Slice/Assets/Scenes - Main/Scripts - main/PowerUpAudio.cs
+++ Vertical Slice/Assets/Scenes - Main/Scripts - main/PowerUpAudio.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PowerUpAudio : MonoBehaviour
 {
-    [SerializeField] private AudioClip pickupSound;
+    [SerializeField] private AudioClip powerUpSound;
 
     private AudioSource audioSource;
 
@@ -13,11 +13,13 @@ public class PowerUpAudio : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
     }
 
-    public void PlayPickupSound()
+    public void PlayPowerUpSound()
     {
-        if (audioSource != null && pickupSound != null)
+        Debug.Log("Playing powerup sound");
+
+        if (audioSource != null && powerUpSound != null)
         {
-            audioSource.PlayOneShot(pickupSound);
+            audioSource.PlayOneShot(powerUpSound);
         }
     }
 }
